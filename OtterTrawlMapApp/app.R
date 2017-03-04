74569304157f1fe772c99fa3dcf6e534845fe6db
Arthur Barros <arthur.e.barros@gmail.com>
2017-03-04 11:06:06 -0800

    Add files via upload

---
+++ OtterTrawlMapApp/app.R
@@ -0,0 +1,207 @@
+###Load Packages###
+##always load plyr before dplyr##
+library(plyr)
+library(dplyr)
+library(ggplot2)
+library(lubridate)
+library(shiny)
+library(reshape2)
+library(leaflet)
+library(DT)
+library(shinythemes)
+library(rmarkdown)
+library(webshot)
+library(rsconnect)
+##source the global file with all the necessary data
+source("global.R")
+##set up "water" for the water quality input select for data explorer tab
+water <- c(
+  "Salinity(ppt)" = "BegSurfSalin",
+  "Temperature(°C)" = "BegSurfTemp",
+  "Conductivity(µS)" = "BegSurfCond",
+  "DO%" = "BegSurfDO.",
+  "DO mg/L" = "BegSurfDO.mg.L.")
+##set up "gear" for gear type selection
+gear<- c(
+  "Large Otter Trawl"="LOT",
+  "Small Otter Trawl"="SOT",
+  "Larval Sled"="SLS",
+  "Seine"="SEINE",
+  "20mm"="20mm",
+  "Mysid"="Mysid",
+  "Mid-Water Trawl"="MWT")
+
+####Shiny####
+##UI building
+ui = bootstrapPage(theme = shinytheme("sandstone"),
+                   navbarPage("Bay Area Catch Data",
+                  #1st tab
+                   tabPanel("Interactive Map",
+                            tags$style(type = "text/css", "html, body {width:100%;height:90%}"),
+                            tags$style(type = "text/css", "#map {height: calc(100vh - 120px) !important;}"),
+                   leafletOutput("map"),
+                   tags$div(id="cite",
+                            'Data compiled for ', tags$b('Hobbs Lab Longfin Smelt Survey'), ' by Arthur Barros (2017).'
+                   ),
+                   absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
+                                 draggable = FALSE, top = 60, left = "auto", right = 20, bottom = "auto",
+                                 width = 200, height = "auto",
+                                 h2("Catch Explorer"),
+                                 selectizeInput("species", "Species",
+                                                unique(as.character(data.cpue.melt$CommonName))),
+                                 dateRangeInput("dates","Date range", start="2015-01-01", end="2015-01-31"),
+                                 textOutput("DateRange"),
+                                 selectInput("region","Region",
+                                             c(unique(as.character(data.cpue.melt$Bay.Region)),"All")),
+                                 selectInput("method","Gear Type",gear),
+                                 downloadButton('html_link','Download map'),
+                                 submitButton("Submit"))
+                   ),
+                  #2nd tab
+                   tabPanel("Data Explorer",
+                            fluidRow(
+                              column(3,
+                                     selectizeInput("species2", "Species",
+                                                    unique(as.character(data.cpue.melt$CommonName))),
+                                     selectInput("region2","Region",
+                                                 c(unique(as.character(data.cpue.melt$Bay.Region)),"All")),
+                                     dateRangeInput("dates2", "Date range", start="2015-01-01", end="2015-01-31"),
+                                     selectInput("method2","Gear Type",gear),
+                                     textOutput("DateRange2"),
+                                     selectInput("water","Water Parameter",water),
+                                     submitButton("Submit")
+                                     ),
+                              column(9,
+                                     (plotOutput("plot2", height=300)),
+                                     (plotOutput("plot3", height=300))
+                              ))
+                            )
+))
+server <- function(input, output, session) {
+  ###########Interactive Map##############################################
+  #Reactive expression used to filter out by user selected variables for final data "filtered()"
+  #for map generation
+  filtered<-reactive({
+    ifelse(input$region=="All",filtered.region<-data.cpue.melt,filtered.region<-data.cpue.melt[data.cpue.melt$Bay.Region==input$region,])
+    filtered.species<- filtered.region[filtered.region$CommonName==input$species,]
+    filtered.gear<-filtered.species[filtered.species$Method==input$method,]
+    filtered.dates<-filtered.gear[filtered.gear$Date>=input$dates[1] & filtered.gear$Date<=input$dates[2],]
+    ##take averages of data in date range
+    filtered.dates=filtered.dates%>%
+      group_by_("Bay.Region","Polygon.Station","longitude","latitude","CommonName")%>%
+      summarise(CPUE=mean(CPUE,na.rm=TRUE), BegSurfSalin=mean(BegSurfSalin,na.rm=TRUE), BegSurfTemp=mean(BegSurfTemp, na.rm=TRUE), 
+                BegSurfCond=mean(BegSurfCond, na.rm=TRUE),BegSurfDO.=mean(BegSurfDO.,na.rm=TRUE),
+                BegSurfDO.mg.L.=mean(BegSurfDO.mg.L.,na.rm=TRUE))
+    filtered.date <-  filtered.dates %>% mutate(CPUE = replace(CPUE,CPUE==0,NA))
+  })
+  
+  #produces base map
+  mymap <- reactive({
+    leaflet(filtered()) %>% addProviderTiles("Hydda.Base")%>%
+      fitBounds(~min(longitude)-.005, ~min(latitude)-.005, ~max(longitude)+.005, ~max(latitude)+.005)
+  })
+  #renders map for main panel in ui
+  output$map <- renderLeaflet({
+    mymap()
+  })
+
+  ##Provides date range count
+  output$DateRange <- renderText({
+    paste("Your date range is", 
+          difftime(input$dates[2], input$dates[1], units="days"),
+          "days")
+  })
+  
+  ##Provides date range count2
+  output$DateRange2 <- renderText({
+    paste("Your date range is", 
+          difftime(input$dates2[2], input$dates2[1], units="days"),
+          "days")
+  })
+ 
+  ##this function used to be an observation, switched to function
+  ##in order to allow for downloading in rmarkdown
+   myfun <- function(map){
+     #this "pal" produces the desired colors and bins for distinguishing CPUE
+     pal<-colorBin(
+       palette="Reds",
+       domain=filtered()$CPUE,
+       bins=c(0,.1,1,10,100,1000),
+       pretty = TRUE,
+       na.color="black")
+     
+     #next call populates map with markers based on filtered() data
+       addCircleMarkers(map, data = filtered(),lng = ~longitude, lat = ~latitude,radius=~ifelse(is.na(filtered()$CPUE),2,10),
+                        stroke=TRUE, color="black",weight=2, fillOpacity=1,
+                        fillColor=~pal(filtered()$CPUE),
+                        popup = ~paste("Catch per Minute Towed:", filtered()$CPUE, "<br>",
+                                       "Station:", Polygon.Station,"<br>",
+                                       "Coordinates:", latitude,",",longitude,"<br>"))%>%
+       addLegend("bottomleft", pal=pal, values=filtered()$CPUE, title="Catch Per Minute of Tow",
+                 opacity=1)
+   }
+#here is an observation using leafletProxy to take the above function and run it on our map
+#also have to have the "clear" calls here, as they won't work in the myfun
+ observe({
+   leafletProxy("map")%>%
+     clearControls%>%
+     clearMarkers()%>% myfun()
+ })
+
+#next call handles the download of the pdf, starts by making an html rmarkdown document 
+ output$html_link <- downloadHandler(
+   filename = 'plot.pdf',
+   
+   content = function(file) {
+     src <- normalizePath('mymap.Rmd')
+     
+     # temporarily switch to the temp dir, in case you do not have write
+     # permission to the current working director
+     ##owd <- setwd(tempdir())
+     ##on.exit(setwd(owd)
+     ##############AB:note, had to stop this to allow rmarkdown document to save changes
+     #file.copy(src, 'mymap.Rmd')
+     out <- render('mymap.Rmd',
+                   html_document()
+     )
+     file.rename(out, 'temp.html')
+     #webshot changes the rmarkdown from html to a static pdf
+     webshot("temp.html", file = file, cliprect = "viewport", delay=2)
+   }
+ )
+ ###############Data Explorer##########
+ filtered2<-reactive({
+   ifelse(input$region2=="All",filtered.region2<-data.cpue.melt,filtered.region2<-data.cpue.melt[data.cpue.melt$Bay.Region==input$region2,])
+   filtered.species2<- filtered.region2[filtered.region2$CommonName==input$species2,]
+   filtered.gear2<-filtered.species2[filtered.species2$Method==input$method,]
+   filtered.dates2<-filtered.species2[filtered.species2$Date>=input$dates2[1] & filtered.species2$Date<=input$dates2[2],]
+   ##take averages data in date range
+ })
+ output$plot2<-reactivePlot(function(){
+   p<-ggplot(filtered2(),aes(Polygon.Station,CPUE))+
+   geom_boxplot()+
+     theme_minimal()+
+     ggtitle("Catch")+
+     xlab("station name")+
+     ylab("log10 CPUE (catch per minute tow)")+
+     scale_y_log10()+
+     theme(axis.text.x = element_text(angle = 90, hjust = 1))
+   print(p)
+ })
+
+ output$plot3<-reactivePlot(function(){
+   q<-ggplot(filtered2(),aes_string(x="Polygon.Station", y=input$water))+
+     geom_boxplot()+
+     theme_minimal()+
+     ggtitle("Water Quality")+
+     xlab("station name")+
+     ylab(input$water)+
+     theme(axis.text.x = element_text(angle = 90, hjust = 1))
+   print(q)
+ })
+    
+    # MW: Stop shiny app when closing the browser
+    session$onSessionEnded(stopApp)
+}
+
+shinyApp(ui = ui, server = server)
---
+++ OtterTrawlMapApp/global.R
@@ -0,0 +1,115 @@
+###Load Packages###
+##always load plyr before dplyr##
+library(plyr)
+library(dplyr)
+library(lubridate)
+library(ggplot2)
+library(shiny)
+library(reshape2)
+library(leaflet)
+library(purrr)
+library(rsconnect)
+
+
+##Read Data##
+data=read.table("data/CrossTabData.txt", header=T, sep="\t")
+#drop lat and long fields to replace later from PolygonGPS#
+head(data)
+polygon.gps=read.table("data/PolygonGPS.txt", header=T, sep="\t")
+species.info=read.table("data/tblSpeciesLookup.txt", header=T, sep="\t")
+station.info=read.table("data/tblStationRegions.txt", header=T, sep="\t")
+larval.data=read.table("data/larvaldata2.txt", header=T, sep="\t")
+
+
+##Clean Data##
+#join data#
+data=data%>%
+  rbind.fill(data,larval.data)
+data.location=station.info%>%
+  inner_join(polygon.gps)
+data=data%>%
+  inner_join(data.location)
+head(data)
+
+#format dates#
+data$Date <- as.Date(data$Date , "%m/%d/%Y")
+#order dates and add month/year columns#
+data[order(data$Date, decreasing=TRUE ),]
+data<-data%>%
+  mutate(month = format(Date, "%m"), year = format(Date, "%Y"))
+#select for 2015 and 2016#
+#target=c("2015","2016")
+#data=data%>%
+  #filter(year %in% target)
+head(data)
+
+data.clean=data
+#filter out inverts#
+data.clean <- subset( data.clean, select = -c(AMPHIPOD, AMPHPD, ASIMUS,CALLIA, CMAENA, CMAGIS, CORBIC, CORBUL, CPRODU, CRANFR, CRANNC, CRANNM, CRANSP, DECORC, EXOPAL,HEMIGR, HEPTSTI, HETERO, HOREGO, IOBSOL, ISOPOD, JELLYF, LSTYLI, MACOMA, MCALIF, MSENHO, MUSBLE, MUSCUL, MUSSEN, MYSID, MYSIDR, MYTILS, NUDIBR, OLURID, PALAEM, PHILIN, PILEWR, POLYCH, PPRODU, RHARRI, RIDGEM, SNAILS, SPONGE, TAPJAP, TUNICA, UCINER, UNID.Clam, UNID.CRAB, UNID.NUD, UNID.LARVAL,UNID.GOBY) )
+head(data.clean)
+
+#need to turn all 0 catches to NA values to calculate CPUE properly
+data.clean[data.clean == 0] <- NA
+
+#calculate CPUE (catch per minute towed)#
+#assign these to new fish range#
+#the "which(colnames)" call will display in the console what columns match those 
+which(colnames(data.clean)=="AMESHA")
+which(colnames(data.clean)=="YELSEE")
+data.cpue=data.clean
+data.cpue[,47:110]=data.cpue[,47:110]/data.clean$TowDuration
+head(data.cpue)
+
+#sum up catch data for totals#
+data.cpue$All=rowSums(data.cpue[,47:110], na.rm=T)
+head(data.cpue)
+#rearrange order#
+data.cpue = data.cpue[, c(1:46,119, 47:118)]
+
+
+#change month from number to month name#
+data.cpue$monthname <- months(as.Date(data.cpue$Date))
+head(data.cpue)
+
+##rearrange in longform using reshape2 melt##
+which(colnames(data.cpue)=="All")
+which(colnames(data.cpue)=="YELSEE")
+data.cpue.melt=melt(data.cpue,id.vars=c("ID","Polygon.Station","Method","BegSurfSalin","BegSurfCond","BegSurfTemp","BegSurfDO.mg.L.","BegSurfDO.","Bay.Region","Date","longitude","latitude","Tributary.Marsh"), measure.vars=c(47:111),
+                    variable.name="FishCode",
+                    value.name="CPUE")
+head(data.cpue.melt)
+
+data.cpue.melt=data.cpue.melt[order(data.cpue.melt$FishCode),]
+head(data.cpue.melt)
+
+#join in common names#
+data.cpue.melt=data.cpue.melt%>%
+  inner_join(species.info)
+head(data.cpue.melt)
+
+#change NA values to 0#
+data.cpue.melt$CPUE[is.na(data.cpue.melt$CPUE)]<-0
+
+#average CPUE by month, year and station#
+#not using this anymore due to changing the date system
+#data.melt.average=data.cpue.melt%>%
+  #group_by_("month","Bay.Region","monthname","year","Polygon.Station","latitude","longitude","CommonName","NatInv")%>%
+  #summarise(CPUE=mean(CPUE,na.rm=TRUE), BegSurfSalin=mean(BegSurfSalin,na.rm=TRUE), BegSurfTemp=mean(BegSurfTemp, na.rm=TRUE), 
+            #BegSurfCond=mean(BegSurfCond, na.rm=TRUE),BegSurfDO.=mean(BegSurfDO.,na.rm=TRUE),
+            #BegSurfDO.mg.L.=mean(BegSurfDO.mg.L.,na.rm=TRUE))
+#data.melt.average
+
+#now switch 0 back to na#
+#data.melt.average$CPUE[data.melt.average$CPUE == 0] <- NA
+
+#get bins for CPUE averages#
+#max(data.melt.average$CPUE, na.rm=TRUE)
+#data.melt.average[order(data.melt.average$CPUE, decreasing=TRUE ),]
+#CPUE.bins <- hist(data.melt.average$CPUE,plot=TRUE)
+
+
+###data debugging###
+data.debug=data.clean%>%
+  filter(Method=="SLS")%>%
+  filter(Polygon.Station=="NB-630")%>%
+  filter(Date=="2015-02-15")
