5eff1ad4b86e772f7615c067066e9f1758190fff
Arthur Barros <arthur.e.barros@gmail.com>
2017-03-29 14:15:09 -0700

    removed some fluff

--- ExampleMapApp/global.R
+++ ExampleMapApp/global.R
@@ -11,7 +11,6 @@ data.catch=read.table("data/catch.txt", header= T, sep= "\t")
 fishcodes=read.table("data/fishcodes.txt", header= T, sep= "\t")
 data.stations=read.table("data/stations.txt", header= T, sep= "\t")
 data.tows=read.table("data/towinfo.txt", header= T, sep= "\t")
-data.water=read.table("data/waterinfo.txt", header= T, sep= "\t")
 
 #####inspect elements#
 head(data.stations)
@@ -39,22 +38,17 @@ data.stations$Longitude<-paste(data.stations$negative, data.stations$Longitude)
 data.stations$Longitude <- gsub(" ", "", data.stations$Longitude)
 head(data.stations)
 #keep columns we need#
-keep<-c("Station","AreaCode","Latitude","Longitude")
+keep<-c("Station","Latitude","Longitude")
 data.stations<-data.stations[ , which(names(data.stations) %in% keep)]
 head(data.stations)
-#transform Lat and Long to numeric
+#transform Lat and Long to numeric class
 data.stations<-transform(data.stations, Latitude = as.numeric(Latitude), Longitude = as.numeric(Longitude))
-
-##remove empty lat and long and comments column from data.water
-head(data.water)
-data.water<-subset(data.water, select = -c(Latitude,Longitude,Comments))
 
 ##join tables together
 data<-data.catch%>%
   inner_join(data.stations)%>%
   inner_join(data.tows)%>%
-  inner_join(fishcodes)%>%
-  inner_join(data.water)
+  inner_join(fishcodes)
 head(data)
 
 #format dates#
@@ -62,7 +56,7 @@ sapply(data,class)
 data$Date <- as.Date(data$Date , "%m/%d/%Y")
 
 #keep columns we need#
-keeps<-c("Date","Station","Latitude","Longitude","Fish.Code","Catch","Duration","Common.Name","AreaCode")
+keeps<-c("Date","Station","Latitude","Longitude","Fish.Code","Catch","Duration","Common.Name")
 data<-data[ , which(names(data) %in% keeps)]
 head(data)
 
@@ -70,24 +64,25 @@ head(data)
 data$CPUE=data$Catch/data$Duration
 head(data)
 
-###Next section to calculate "All" cPUE for each tow
+###Next section to calculate "All" CPUE for each tow
 #reshape fish catch to wide format
-datawide <- dcast(data, Station +Latitude+Longitude+AreaCode+Duration+ Date ~ Common.Name, value.var="CPUE",fun.aggregate=sum)
+datawide <- dcast(data, Station +Latitude+Longitude+Duration+ Date ~ Common.Name, value.var="CPUE",fun.aggregate=sum)
 head(datawide)
 #learn which column numbers apply to fish
 which(colnames(datawide)=="American shad")
 which(colnames(datawide)=="yellowfin goby")
 #calculate All
-datawide$All=rowSums(datawide[,7:72], na.rm=T)
+datawide$All=rowSums(datawide[,6:71], na.rm=T)
 head(datawide)
 #melt back straight
-data=melt(datawide,id.vars=c("Station","Date","Longitude","Latitude","AreaCode"), measure.vars=c(7:73),
+data=melt(datawide,id.vars=c("Station","Date","Longitude","Latitude"), measure.vars=c(6:72),
              variable.name="Common.Name",
              value.name="CPUE")
-data=data[order(data$Common.Name),]
 #rename Lat and Lon for leaflet
 data<-rename(data, latitude=Latitude, longitude=Longitude)
 head(data)
+##reorder alphabetically by common name
+data=data[order(data$Common.Name),]
 
 ##debug section
 debug<-data%>%
