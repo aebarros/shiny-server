6ff38c1acb43483bb098f9a788fade7df1ff2917
Arthur Barros <arthur.e.barros@gmail.com>
2017-12-28 12:05:22 -0800

    jitter coordinates

--- CBMapApp/global.R
+++ CBMapApp/global.R
@@ -1,7 +1,11 @@
 ###Load Packages###
 
 library(dplyr)
+library(sp)
 
 
 data.cb=readRDS("data/cbdata.rds")
+head(data.cb)
+data.cb$latitude<-jitter(data.cb$latitude,factor=10)
+data.cb$longitude<-jitter(data.cb$longitude,factor=10)
 head(data.cb)
